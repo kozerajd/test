bff1b00982fe34fd1adfc9394fe5b4aefc9c5d3c
kozerajd <129620369+kozerajd@users.noreply.github.com>
2023-04-12 13:38:44 -0400

    Update montecarloclassicalparticle.py

---
+++ montecarloclassicalparticle.py
@@ -0,0 +1,34 @@
+import random
+
+# target coordinates
+target_x = 5
+target_y = 5
+
+# radius of the target
+target_radius = 1
+
+# number of darts to throw
+num_darts = 100000000
+
+# initialize number of darts that hit the target
+hits = 0
+
+for i in range(num_darts):
+    # generate random x and y coordinates for the dart
+    dart_x = random.uniform(0, 10)
+    dart_y = random.uniform(0, 10)
+
+    # calculate the distance between the dart and the target
+    distance = ((dart_x - target_x) ** 2 + (dart_y - target_y) ** 2) ** 0.5
+
+    # check if the dart hit the target
+    if distance <= target_radius:
+        hits += 1
+
+# calculate the proportion of darts that hit the target
+hit_proportion = hits / num_darts
+
+# calculate the estimate of the area of the target
+estimate = hit_proportion * (10 ** 2)
+
+print("Estimate of the area of the target:", estimate)
